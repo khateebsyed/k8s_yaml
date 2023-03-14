465f86390e5e228602618a2b09d936f22c61626f
khateebsyed <124815410+khateebsyed@users.noreply.github.com>
2023-03-14 22:48:49 +0530

    Add files via upload
    k8s_setup_files

---
+++ k8s_cluster-setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Update the system
+sudo apt update ; sudo apt upgrade -y
+
+# Install necessary software
+sudo apt install curl apt-transport-https vim git wget gnupg2 software-properties-common apt-transport-https ca-certificates -y
+
+# Add repo for Kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install the Kubernetes software, and lock the version
+sudo apt update
+sudo apt -y install kubelet=1.26.1-00 kubeadm=1.26.1-00 kubectl=1.26.1-00
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Ensure Kubelet is running
+sudo systemctl enable --now kubelet
+
+# Disable swap just in case
+sudo swapoff -a
+
+# Ensure Kernel has modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Update networking to allow traffic
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system
+
+# Configure containerd settings
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo sysctl --system
+
+# Install the containerd software
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install containerd.io -y
+
+# Configure containerd and restart
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
